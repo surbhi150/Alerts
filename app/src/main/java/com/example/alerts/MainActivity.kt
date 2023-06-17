8751a2458f2e37ca3a7d22b497ebfb92af6dc7c2
surbhi150 <135599566+surbhi150@users.noreply.github.com>
2023-06-17 10:47:26 +0530

    Alert Dialog Box

--- app/src/main/java/com/example/alerts/MainActivity.kt
+++ app/src/main/java/com/example/alerts/MainActivity.kt
@@ -4,30 +4,34 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.alerts.databinding.ActivityMainBinding
+import com.example.alerts.databinding.CustomDialogueBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnSnackbar : Button
-    lateinit var btnAlert : Button
-    lateinit var btnCustom : Button
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnSnackbar = findViewById(R.id.btnSnackbar)
-        btnAlert = findViewById(R.id.btnAlert)
-        btnCustom = findViewById(R.id.btnCustom)
-        btnSnackbar.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-           var snackbarView = Snackbar.make(btnSnackbar,"This is snackbar",Snackbar.LENGTH_LONG)
+
+       binding.btnSnackbar.setOnClickListener {
+
+           var snackbarView = Snackbar.make(binding.btnSnackbar,"This is snackbar",Snackbar.LENGTH_LONG)
             snackbarView.show()
             snackbarView.setAnchorView(R.id.btnSnackbar)
             snackbarView.setAction("OK"){
                 Toast.makeText(this,"This is toast",Toast.LENGTH_LONG).show()
             }
         }
-        btnAlert.setOnClickListener {
+        binding.btnAlert.setOnClickListener {
             AlertDialog.Builder(this)
                 .setTitle("Alert Title")
                 .setMessage("This is alert")
@@ -46,16 +50,25 @@ class MainActivity : AppCompatActivity() {
                 }
                 .show()
         }
-        btnCustom.setOnClickListener {
+        binding.btnCustom.setOnClickListener {
             var dialog = Dialog(this)
             dialog.setContentView(R.layout.custom_dialogue)
             dialog.show()
             var btnSet = dialog.findViewById<Button>(R.id.btnSet)
             var btnCancel = dialog.findViewById<Button>(R.id.btnCancel)
+
+            var etEnterHere = dialog.findViewById<EditText>(R.id.etEnterHere)
+            etEnterHere.setText(binding.tvHello.text)
+
+
             btnSet.setOnClickListener {
+                if (etEnterHere.text.toString().isNullOrEmpty()){
+                    etEnterHere.error = "Enter name"
+                }else{
+                    binding.tvHello.setText(etEnterHere.text.toString())
                 Toast.makeText(this,"Set",Toast.LENGTH_LONG)
                     .show()
-                dialog.dismiss()
+                dialog.dismiss()}
             }
             btnCancel.setOnClickListener {
                 Toast.makeText(this,"Cancelled",Toast.LENGTH_LONG)
